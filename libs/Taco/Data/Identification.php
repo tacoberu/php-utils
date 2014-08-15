a346bf15e0be8cb238c135d8f06564cc670909ec
Martin Takáč <martin@takac.name>
2014-08-16 01:35:47 +0200

    add: Identification inteface.

---
+++ libs/Taco/Data/Identification.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * This file is part of the Taco Projects.
+ *
+ * Copyright (c) 2004, 2013 Martin Takáč (http://martin.takac.name)
+ *
+ * For the full copyright and license information, please view
+ * the file LICENCE that was distributed with this source code.
+ *
+ * PHP version 5.3
+ *
+ * @author     Martin Takáč (martin@takac.name)
+ */
+
+namespace Taco\Data;
+
+
+
+interface Identification
+{
+
+	/**
+	 * Representation ident as string.
+	 * @return string
+	 */
+	function getIdentKey();
+
+}
