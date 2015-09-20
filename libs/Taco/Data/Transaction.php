43424e22c8450c4ebbd83c1efba274e40a0ef24c
Martin Takáč <martin@takac.name>
2015-09-20 20:41:34 +0200

    Add: Transaction interface.

---
+++ libs/Taco/Data/Transaction.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * This file is part of the Taco Projects.
+ *
+ * Copyright (c) 2004, 2013 Martin Takáč (http://martin.takac.name)
+ *
+ * For the full copyright and license information, please view
+ * the file LICENCE that was distributed with this source code.
+ *
+ * PHP version 5.3
+ *
+ * @author     Martin Takáč (martin@takac.name)
+ */
+
+namespace Taco\Data;
+
+
+interface Transaction
+{
+
+	function commitTransaction();
+
+
+	function rollbackTransaction();
+
+
+}
