108237442e284224c12bca843389eb3662cf1c01
Martin Takáč <martin@takac.name>
2017-08-25 01:26:34 +0200

    TextParser::indexOfQuotes
    clear code

--- libs/Taco/Parsers/TextParser.php
+++ libs/Taco/Parsers/TextParser.php
@@ -1,19 +1,10 @@
 <?php
 /**
- * This file is part of the Taco Projects.
- *
- * Copyright (c) 2004, 2013 Martin Takáč (http://martin.takac.name)
- *
- * For the full copyright and license information, please view
- * the file LICENCE that was distributed with this source code.
- *
- * PHP version 5.3
- *
- * @author     Martin Takáč (martin@takac.name)
+ * Copyright (c) since 2004 Martin Takáč
+ * @author Martin Takáč <martin@takac.name>
  */
 
 namespace Taco\Parsers;
-
 
 use Nette\Utils,
 	Nette\Reflection;
@@ -24,16 +15,18 @@ use RuntimeException,
 class TextParser
 {
 
+	const QUOTE      = "\x22";
+	const APOSTROPHE = "\x27";
+	const BACKSLASH  = "\x5c";
 
-	private static $BRACKET_MAP = array(
-			'(' => ')',
-			'[' => ']',
-			'{' => '}',
-			);
-
+	const ESCAPE_STYLE_NONE = null;
+	const ESCAPE_STYLE_BACKSLASH = 1;
+	const ESCAPE_STYLE_DOUBLE = 2;
 
 
 	/**
+	 * @deprecated
+	 *
 	 * Vyhledá konec řetězce. Bere v potaz escapování.
 	 * Předpokládá se, že řetězec je otevřený, tedy, že na indexu -1 byla
 	 * uvozovka poznačená v $char
@@ -57,6 +50,8 @@ class TextParser
 
 
 	/**
+	 * @deprecated
+	 *
 	 * Vyhledá konec výrazu ukončený závorkou. Počítá otevírací závorky.
 	 * Předpokládá se, že výraz je otevřený, tedy, že na indexu -1 byla otevírací závorka.
 	 *
@@ -66,13 +61,18 @@ class TextParser
 	 */
 	function lookupEndBracketIndex($source, $bracket = ')')
 	{
+		$BRACKET_MAP = array(
+			'(' => ')',
+			'[' => ']',
+			'{' => '}',
+		);
 		$index = 0;
 		while ($xs = Utils\Strings::match($source, '~[\(\)\{\}\[\]]~is', PREG_OFFSET_CAPTURE, $index)) {
 			if ($xs[0][0] == $bracket) {
 				return $xs[0][1] + 1;
 			}
 			$index = $xs[0][1];
-			if (($shift = $this->lookupEndBracketIndex(substr($source, $index + 1), self::$BRACKET_MAP[$xs[0][0]])) === False) {
+			if (($shift = $this->lookupEndBracketIndex(substr($source, $index + 1), $BRACKET_MAP[$xs[0][0]])) === False) {
 				return False;
 			}
 			$index += $shift + 1;
@@ -80,5 +80,106 @@ class TextParser
 		return False;
 	}
 
+
+
+	/**
+	 * Hledáme uvozovku (určenou prvním argumentem). Zohledňujeme escapování. Způsob escapování určujeme explicitně.
+	 * @return int
+	 */
+	static function indexOfQuotes($quote, $s, $offset = 0, $escapedstyle = self::ESCAPE_STYLE_BACKSLASH)
+	{
+		switch ($escapedstyle) {
+			case self::ESCAPE_STYLE_BACKSLASH:
+				$escaped = self::BACKSLASH . $quote;
+				break;
+			case self::ESCAPE_STYLE_DOUBLE:
+				$escaped = $quote . $quote;
+				break;
+			default:
+				throw new InvalidArgumentException("Unsupported style of escaping.");
+		}
+		$res = self::findQuotes($quote, $escaped, $s, $offset);
+		if ($res === False) {
+			return -1;
+		}
+		return $res[1];
+	}
+
+
+
+	/**
+	 * Nejbližší neescapovaná uvozovka nebo apostrof.
+	 * @return [", abs-pos] | [', abs-pos] | False
+	 */
+	static function indexOfText($src, $offset = 0, $escapedstyle = self::ESCAPE_STYLE_BACKSLASH) {
+		$a = self::indexOfQuotes(self::QUOTE,      $src, $offset, $escapedstyle);
+		$b = self::indexOfQuotes(self::APOSTROPHE, $src, $offset, $escapedstyle);
+		if ($a >= 0 && $b >= 0) {
+			return $a > $b ? [self::APOSTROPHE, $b] : [self::QUOTE, $a];
+		}
+
+		if ($a >= 0) {
+			return [self::QUOTE, $a];
+		}
+
+		if ($b >= 0) {
+			return [self::APOSTROPHE, $b];
+		}
+
+		return False;
+	}
+
+
+
+	/**
+	 * Hledáme uvozovku (určenou prvním argumentem). Způsob escapování určujeme explicitně.
+	 * @return [", abs-pos] | [', abs-pos] | False
+	 */
+	private static function findQuotes($quote, $escaped, $s, $offset = 0)
+	{
+		// @HACK - předpokládáme, že escapování je řešeno pomocí jednoho znaku. Což třeba v přípaě SQL delimiteru neplatí.
+		// Ale dá se prohlásit, že v případě SQL delimiterů prostě neescapujeme.
+		$escapedofescaped = $escaped{0};
+		$pos = strpos($s, $quote,   $offset);
+		$esc = strpos($s, $escaped, $offset);
+
+		// Ověřit, zda není escapované escapování. Což může být kumulativní.
+		if ($esc !== False) {
+			$use = True;
+			for ($i = $esc - 1; $i >= 0; $i--) {
+				if ($s[$i] !== $escapedofescaped) {
+					break;
+				}
+				$use = ! $use;
+			}
+			if ( ! $use) {
+				$esc = False;
+			}
+
+			$i += 1; // posunout index na začátek
+
+			// Nejdřív se musím posunout za escapované escapování.
+			while (True) {
+				$i += 2;
+				$x = strpos($s, $escapedofescaped . $escapedofescaped, $i);
+				if ($x === False) {
+					break;
+				}
+			}
+			$pos = strpos($s, $quote,   $i);
+			$esc = strpos($s, $escaped, $i);
+		}
+
+		// Escapovaná uvozovka.
+		if ($esc !== False && $pos !== False && $esc <= $pos) {
+			return self::findQuotes($quote, $escaped, $s, $pos + strlen($escaped));
+		}
+
+		if ($pos !== False) {
+			return [$quote, $pos];
+		}
+
+		return False;
+	}
 
 }
--- tests/Taco/Parsers/TextParserTest.php
+++ tests/Taco/Parsers/TextParserTest.php
@@ -1,28 +1,13 @@
 <?php
 /**
- * This file is part of the Taco Projects.
- *
- * Copyright (c) 2004, 2013 Martin Takáč (http://martin.takac.name)
- *
- * For the full copyright and license information, please view
- * the file LICENCE that was distributed with this source code.
- *
- * PHP version 5.3
- *
- * @author     Martin Takáč (martin@takac.name)
+ * Copyright (c) since 2004 Martin Takáč
+ * @author Martin Takáč <martin@takac.name>
  */
-
 
 namespace Taco\Parsers;
 
-
-require_once __dir__ . '/../../../vendor/autoload.php';
-require_once __dir__ . '/../../../libs/Taco/Parsers/TextParser.php';
-
-
 use PHPUnit_Framework_TestCase;
 use Nette;
-
 
 
 /**
@@ -39,7 +24,7 @@ class TextParserTest extends PHPUnit_Framework_TestCase
 		$this->parser = new TextParser();
 	}
 
-/*
+
 
 	function testLookupEndQuoteIndexEmpty()
 	{
@@ -55,59 +40,144 @@ class TextParserTest extends PHPUnit_Framework_TestCase
 
 
 
-	function testLookupEndQuoteIndexWithoutEnd()
-	{
-		$this->assertFalse($this->parser->lookupEndIndex('abc def', '"'));
-	}
-
-
-
-	function testLookupEndQuoteIndexSample()
-	{
-		$this->assertEquals(7, $this->parser->lookupEndIndex('abc def"', '"'));
-	}
-
-
-
-	function testLookupEndQuoteWithEscapedQuote()
-	{
-		$source = "a bc def 123 'pi' \\\"sss \"da next tex";
-		$this->assertEquals(24, $this->parser->lookupEndIndex($source, '"'));
-	}
-
-
-	function testLookupEndBracketIndexEmpty()
-	{
-		$this->assertFalse($this->parser->lookupEndBracketIndex(''));
-	}
-
-
-
-	function testLookupEndBracketIndexShort()
-	{
-		$this->assertEquals(0, $this->parser->lookupEndBracketIndex(')'));
-	}
-
-
-
-	function testLookupEndBracketIndexWithoutEnd()
-	{
-		$this->assertFalse($this->parser->lookupEndBracketIndex('abc def', '"'));
-	}
-
-
-
-	function testLookupEndBracketIndexSample()
-	{
-		$this->assertEquals(7, $this->parser->lookupEndBracketIndex('abc def)'));
-	}
-*/
-
 	function testLookupEndBracketIndexA()
 	{
 		$this->assertEquals(15, $this->parser->lookupEndBracketIndex('abc (defx) ddd) vvv'));
 		$this->assertEquals(19, $this->parser->lookupEndBracketIndex('abc (defx) (bb)ddd) vvv'));
 	}
 
+
+
+	/**
+	 * @dataProvider dataFindQuotesBackslash
+	 */
+	function testIndexOfQuotesBackslash($except, $src, $offset)
+	{
+		$this->assertEquals($except, TextParser::indexOfQuotes(TextParser::QUOTE, $src, $offset));
+	}
+
+
+
+	function dataFindQuotesBackslash()
+	{
+		return [
+			[-1, 'abcdefghijklm', 0], // nic
+			[ 3, 'abc"def"ghi"jklm', 0], // více možností
+			[ 8, 'abc\"def"ghi"jklm', 0], // vynechání escapované
+			[ 8, 'abc\"def"ghi"jklm', 8], // escapovanou uvozovku přeskočím offsetem
+			[12, 'abc\"def"ghi"jklm', 9], // první dvě uvozvoky přeskočím offsetem
+			[ 0, '"abcdefghi"jklm', 0], // uplně na začátku, bejvá zrada
+			[11, '\"abcdefghi"jklm', 0], // uplně na začátku
+			[ 5, 'abc' . TextParser::BACKSLASH . TextParser::BACKSLASH . '"def"ghi"jklm', 0], // escapování escapování ruší escapování
+			[10, 'abc' . TextParser::BACKSLASH . TextParser::BACKSLASH . TextParser::BACKSLASH . '"def"ghi"jklm', 0], // escapování escapování escapování ruší rušení escapování
+			[ 2, '\\\"abcdefghi"jklm', 0], // uplně na začátku
+			[ 2, '\\\"abcdefghi"jklm', 1],
+			[ 2, '\\\"abcdefghi"jklm', 2],
+			[12, '\\\"abcdefghi"jklm', 3],
+			[ 5, 'abcde""fghi"jklm', 0], // escapování zdvojením se ignoruje
+			[ 6, 'abc\'de""f\'gh\'i"jklm', 0], // ostatní uvozovky ignoruje
+		];
+	}
+
+
+
+	/**
+	 * @dataProvider dataFindQuotesDouble
+	 */
+	function testIndexOfQuotesDouble($except, $src, $offset)
+	{
+		$this->assertEquals($except, TextParser::indexOfQuotes(TextParser::QUOTE, $src, $offset, TextParser::ESCAPE_STYLE_DOUBLE));
+	}
+
+
+
+	function dataFindQuotesDouble()
+	{
+		return [
+			[-1, 'abcdefghijklm', 0], // nic
+			[ 3, 'abc"def"ghi"jklm', 0], // více možností
+			[ 8, 'abc""def"ghi"jklm', 0], // vynechání escapované
+			[ 8, 'abc""def"ghi"jklm', 8],
+			[12, 'abc""def"ghi"jklm', 9],
+			[12, 'abc""def"ghi"jklm', 10],
+			[ 0, '"abcdefghi"jklm', 0], // uplně na začátku
+			[11, '""abcdefghi"jklm', 0], // uplně na začátku
+			[ 5, 'abc"""def"ghi"jklm', 0], // escapování escapování ruší escapování
+			[10, 'abc""""def"ghi"jklm', 0], // escapování escapování escapování ruší rušení escapování
+			[ 2, '"""abcdefghi"jklm', 0], // uplně na začátku
+			[ 2, '"""abcdefghi"jklm', 1],
+			[ 2, '"""abcdefghi"jklm', 2],
+			[12, '"""abcdefghi"jklm', 3],
+			[ 6, 'abcde\"fghi"jklm', 0], // escapování lomítkem se ignoruje
+			[14, 'abc\'de""f\'gh\'i"jklm', 0], // ostatní uvozovky ignoruje
+		];
+	}
+
+
+
+	/**
+	 * @dataProvider dataFindQuotesWithoutEscaping
+	 */
+	function _testFindQuotesWithoutEscaping($except, $src, $offset)
+	{
+		$this->assertEquals($except, TextParser::indexOfQuotes(TextParser::QUOTE, $src, $offset, TextParser::ESCAPE_STYLE_NONE));
+	}
+
+
+
+	function dataFindQuotesWithoutEscaping()
+	{
+		return [
+			[false,     'abcdefghijklm', 0], // nic
+			[['"',  3], 'abc"def"ghi"jklm', 0], // více možností
+			[['"',  8], 'abc""def"ghi"jklm', 0], // vynechání escapované
+			[['"',  8], 'abc""def"ghi"jklm', 8],
+			[['"', 12], 'abc""def"ghi"jklm', 9],
+			[['"', 12], 'abc""def"ghi"jklm', 10],
+			[['"',  0], '"abcdefghi"jklm', 0], // uplně na začátku
+			[['"', 11], '""abcdefghi"jklm', 0], // uplně na začátku
+			[['"',  5], 'abc"""def"ghi"jklm', 0], // escapování escapování ruší escapování
+			[['"', 10], 'abc""""def"ghi"jklm', 0], // escapování escapování escapování ruší rušení escapování
+			[['"',  2], '"""abcdefghi"jklm', 0], // uplně na začátku
+			[['"',  2], '"""abcdefghi"jklm', 1],
+			[['"',  2], '"""abcdefghi"jklm', 2],
+			[['"', 12], '"""abcdefghi"jklm', 3],
+			[['"',  6], 'abcde\"fghi"jklm', 0], // escapování lomítkem se ignoruje
+		];
+	}
+
+
+
+	/**
+	 * @dataProvider dataIndexOfText
+	 */
+	function testIndexOfText($except, $src, $offset)
+	{
+		$this->assertEquals($except, TextParser::indexOfText($src, $offset));
+	}
+
+
+
+	function dataIndexOfText()
+	{
+		return [
+			[false,     'abcdefghijklm', 0], // nic
+			[['"',  3], 'abc"def"ghi"jklm', 0], // více možností
+			[['"',  8], 'abc\"def"ghi"jklm', 0],
+			[['"',  8], 'abc\"def"ghi"jklm', 8],
+			[['"', 12], 'abc\"def"ghi"jklm', 9],
+			[['"', 12], 'abc\"def"ghi"jklm', 10],
+			[['"',  0], '"abcdefghi"jklm', 0], // uplně na začátku
+			[['"', 11], '\"abcdefghi"jklm', 0], // uplně na začátku
+			[['"',  8], 'abc\"def"ghi"jklm', 0],
+			[['"',  5], 'abc\\\"def"ghi"jklm', 0],
+			[['"',  2], '\\\\"abcdefghi"jklm', 0], // uplně na začátku
+			[['"',  2], '\\\\"abcdefghi"jklm', 1],
+			[['"',  2], '\\\\"abcdefghi"jklm', 2],
+			[['"', 12], '\\\\"abcdefghi"jklm', 3],
+			[["'",  3], 'abc\'def"ghi"jklm', 0],
+			[['"',  8], 'abc\\\'def"ghi"jklm', 0],
+		];
+	}
 
 }
