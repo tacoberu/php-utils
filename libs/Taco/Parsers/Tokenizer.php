4ee22648e49456533a5fbcf208aa255b2ac6859a
Martin Takáč <martin@takac.name>
2017-09-04 18:23:13 +0200

    Tokenizer

---
+++ libs/Taco/Parsers/Tokenizer.php
@@ -0,0 +1,246 @@
+<?php
+/**
+ * Copyright (c) since 2004 Martin Takáč
+ * @author Martin Takáč <martin@takac.name>
+ */
+
+namespace Taco\Parsers;
+
+use RuntimeException;
+
+
+/**
+ * Rozbije to text na tokeny oddělené mezerou. Přičemž zachovává sekvence uvozené uvozovkama a apostrofama.
+ * Pokud máme nějaký text ohraničený tagy, tak z něj udělá subtoken.
+ * Nemáme zájem nějak složitěji mapovat nebo zpracovávat jednotlivé tokeny. Jde nám čistě a
+ * pouze o ohraničení jednotlivých fragmentů. Aby se nehledali tagy v textu.
+ *
+ * @TODO Tagy jako taky strom.
+ * @TODO Ukončení více možnými tagy.
+ * @TODO Sebeukončovací tag. Tzn, že tag skončí tím, že začne jiný.
+ * @TODO Nezvládne text formátovaný odsazováním aka python.
+ */
+class Tokenizer
+{
+
+	/**
+	 * @var array of pair of string
+	 */
+	private $tags;
+
+
+	function __construct($tags = [['(', ')']])
+	{
+		$this->tags = $tags;
+	}
+
+
+
+	function tokenize($src)
+	{
+		$def = $this->buildTree2($src, 0);
+		return $def[0]->getItems();
+	}
+
+
+
+	/**
+	 * Rozbije to text na tokeny oddělené mezerou. Přičemž zachovává sekvence uvozené uvozovkama a apostrofama.
+	 *
+	 * @TODO Není to úplně přesný. Pro řetězec ~abc def"veta"pok xyz~ to na urovni uvozovek zalomí.
+	 */
+	static function parse($src) {
+		$res = [];
+		// Najdeme první uvozovku. Kod do ní zpracujeme.
+		while ($quote = TextParser::indexOfText($src)) {
+			$head = substr($src, 0, $quote[1]);
+			$res = array_merge($res, self::split(rtrim($head)));
+			$src = substr($src, $quote[1]);
+
+			// Najdeme druhou uvozovku.
+			$index = TextParser::indexOfQuotes($quote[0], $src, 1);
+			if ($index < 0) {
+				break;
+			}
+			$res[] = substr($src, 0, $index + 1);
+			$src = trim(substr($src, $index + 1));
+		}
+
+		if ($src) {
+			$res = array_merge($res, self::split($src));
+		}
+
+		return array_values(array_filter($res));
+	}
+
+
+
+	private function buildTree2($src, $offset, $open = null, $close = null)
+	{
+		$res = [];
+		while ($src) {
+			$tag = $this->findTag($src, $offset);
+			$quote = TextParser::indexOfText($src, $offset);
+
+			// text má přednost
+			if ($quote && $quote[1] < $tag[0]) {
+				$index = TextParser::indexOfQuotes($quote[0], $src, $quote[1] + 1);
+				if ($index < 0) {
+					break;
+				}
+				$offset = $index + 1;
+			}
+			else {
+				switch (True) {
+					// Zanoření
+					case (count($tag) === 3):
+						if ($tag[0]) {
+							$res[] = substr($src, 0, $tag[0]);
+						}
+						$src = substr($src, $tag[0] + strlen($tag[1]));
+						$def = $this->buildTree2($src, 0, $tag[1], $tag[2]);
+						list($tree, $tail) = $def;
+						$res[] = $tree;
+						$src = $tail;
+						break;
+
+					// Uzavření
+					case (count($tag) === 2):
+						//~ self::assertCloseTag($src, $tag[0], $tag[1]);
+						if ($x = substr($src, 0, $tag[0])) {
+							$res[] = $x;
+						}
+						$tail = substr($src, $tag[0] + strlen($close));
+						if (empty($tail)) {
+							$tail = false;
+						}
+
+						return [new Token($open, $res, $close), $tail];
+
+					// Žádný další tag
+					case ($tag === False):
+						$res[] = $src;
+						return [new Token($open, $res, $close), false];
+				}
+			}
+		}
+
+		return [new Token($open, $res, $close), false];
+	}
+
+
+
+	/**
+	 * Nejbližší tag. Otevírací či zavírací.
+	 * @return [abs-pos, open, close] |  [abs-pos, close] | false
+	 */
+	private function findTag($src, $offset) {
+		$curr = false;
+		foreach ($this->tags as $pair) {
+			list($open, $close) = $pair;
+			$index = strpos($src, $open, $offset);
+
+			// tag je nalezen, a buď je to první nalezený, nebo je lepší jak první nalezený.
+			if ($index !== false && ( ! $curr || ($index < $curr[0]))) {
+				$curr = [$index, $open, $close];
+			}
+
+			// A co když je uzavírací tag ještě dříve?
+			$index = strpos($src, $close, $offset);
+			if ($index !== false && ( ! $curr || ($index < $curr[0]))) {
+				$curr = [$index, $close];
+			}
+		}
+
+		return $curr;
+	}
+
+
+
+	private static function split($s)
+	{
+		return explode(' ', $s);
+	}
+
+
+
+	/**
+	 * @param string
+	 * @param int
+	 * @param string
+	 * PHP Parse error:  syntax error, unexpected ')' in /home/dell/Projects/php-utils.github/tests/Taco/Parsers/TokenizerTest.php on line 58
+	 */
+	private static function assertOpenTag($src, $pos, $tag)
+	{
+		if (substr($src, $pos, strlen($tag)) !== $tag) {
+			$fragment = substr($src, -80);
+			throw new RuntimeException("Cannot found open tag: `{$tag}' in fragment: `...{$fragment}'.");
+		}
+	}
+
+
+
+	/**
+	 * @param string
+	 * @param int
+	 * @param string
+	 */
+	private static function assertCloseTag($src, $pos, $tag)
+	{
+		if (substr($src, $pos, strlen($tag)) !== $tag) {
+			$fragment = substr($src, -80);
+			throw new RuntimeException("Cannot found close tag: `{$tag}' in fragment: `...{$fragment}'.");
+		}
+	}
+
+}
+
+
+
+class Token
+{
+	private $open;
+	private $close;
+	private $items;
+
+	/**
+	 * @param string $open Otevírací token. Například <tag>. Netýká se stringu. Otevírací tag může být opět token.
+	 * @param array $items Mohou to být stringy, nebo další Tokeny. Tedy, vytváříme strom.
+	 * @param string $close Uzavírací token jako </>
+	 */
+	function __construct($open, array $items, $close)
+	{
+		$this->open = $open;
+		$this->close = $close;
+		$this->items = $items;
+	}
+
+
+
+	function getItems()
+	{
+		return $this->items;
+	}
+
+
+
+	function getOpen()
+	{
+		return $this->open;
+	}
+
+
+
+	function getClose()
+	{
+		return $this->close;
+	}
+
+
+
+	function __toString()
+	{
+		return $this->open . implode('', $this->items) . $this->close;
+	}
+
+}
---
+++ tests/Taco/Parsers/TokenizerTest.php
@@ -0,0 +1,437 @@
+<?php
+/**
+ * Copyright (c) since 2004 Martin Takáč
+ * @author Martin Takáč <martin@takac.name>
+ */
+
+namespace Taco\Parsers;
+
+use PHPUnit_Framework_TestCase;
+use Nette\Utils;
+
+
+class TokenizerTest extends PHPUnit_Framework_TestCase
+{
+
+	function testParseQuotes() {
+		$src = 'abx cdx \"efx \\\'ghx aaa"chx ijk" lm ne "a jak se" máš';
+		$this->assertEquals([
+			'abx', 'cdx', '\"efx', '\\\'ghx', 'aaa', '"chx ijk"', 'lm', 'ne', '"a jak se"', 'máš'
+		], Tokenizer::parse($src));
+	}
+
+
+
+	function testParseWhiteChars() {
+		$src = 'abx cdx';
+		$this->assertEquals([
+			'abx', 'cdx'
+		], Tokenizer::parse($src));
+	}
+
+
+
+	function testParseManyWhiteChars() {
+		$src = 'abx  cdx';
+		$this->assertEquals([
+			'abx', 'cdx'
+		], Tokenizer::parse($src));
+	}
+
+
+
+	/**
+	 * @dataProvider dataTokenize
+	 */
+	function testTokenize($except, $src)
+	{
+		$this->assertEquals($except, (new Tokenizer())->tokenize($src));
+		$this->assertEquals($src, implode('', (new Tokenizer())->tokenize($src)));
+	}
+
+
+
+	function dataTokenize()
+	{
+		return [
+			[['abcdefghijklm']
+				, 'abcdefghijklm'],
+			[[new Token('(', ['defghi'], ')')]
+				, '(defghi)'],
+			[['aábcč', new Token('(', ['dďeéěfghií'], ')'), ' jklmnňoópqrřsštťuvwxyýzž',]
+				, 'aábcč(dďeéěfghií) jklmnňoópqrřsštťuvwxyýzž'],
+			[[
+				'aábcč',
+				new Token('(', ['dďeéěfghií'], ')'),
+				' jklm',
+				new Token('(', ['nňo'], ')'),
+				'ópqrřsštťuvwxyýzž'
+				]
+				, 'aábcč(dďeéěfghií) jklm(nňo)ópqrřsštťuvwxyýzž'],
+			[['abcde\"fghijklm']
+				, 'abcde\"fghijklm'],
+			[[
+				'aábcč',
+				new Token('(', [
+					'dďeéěfghií',
+					new Token('(', ['jklmnň'], ')'),
+					'oópqrřs',
+					], ')'),
+				'štťuvwxyýzž']
+				, 'aábcč(dďeéěfghií(jklmnň)oópqrřs)štťuvwxyýzž'],
+			[['abcde"fgh"ijklm']
+				, 'abcde"fgh"ijklm'],
+			[[
+				'abc',
+				new Token('(', ['de"fgh"ijk'], ')'),
+				'lm']
+				, 'abc(de"fgh"ijk)lm'],
+			[['abc',
+				new Token('(', ['de\"fgh\"ijk'], ')'),
+				'lm']
+				, 'abc(de\"fgh\"ijk)lm'],
+			[['abc',
+				new Token('(', [
+					'de\"f',
+					new Token('(', ['gh'], ')'),
+					'"ij"k'], ')'),
+				'lm',
+				]
+				, 'abc(de\"f(gh)"ij"k)lm'],
+		];
+	}
+
+
+
+	function testTokenize1() {
+		$src = 'abc (ab1 x bc2) def [i jkl] mn';
+		$tokenizer = (new Tokenizer([['(', ')'], ['[', ']']]));
+		$this->assertEquals([
+			'abc ',
+			new Token('(', ['ab1 x bc2'], ')'),
+			' def ',
+			new Token('[', ['i jkl'], ']'),
+			' mn',
+		], $tokenizer->tokenize($src));
+		$this->assertEquals($src, implode('', $tokenizer->tokenize($src)));
+	}
+
+
+
+	function testTokenize2() {
+		$src = 'abx (ab1 bc2) def (a58) kjl';
+		$this->assertEquals([
+			'abx ',
+			new Token('(', ['ab1 bc2'], ')'),
+			' def ',
+			new Token('(', ['a58'], ')'),
+			' kjl',
+		], (new Tokenizer())->tokenize($src));
+		$this->assertEquals($src, implode('', (new Tokenizer())->tokenize($src)));
+	}
+
+
+
+	function testTokenize3_fail() {
+		$src = 'abx {if}ab1 bc2{/} def (a58) kjl';
+		$tokenizer = new Tokenizer([
+			['{if}', '{/}'],
+			['(', ')'],
+		]);
+
+		$this->assertEquals([
+			'abx ',
+			new Token('{if}', ['ab1 bc2'], '{/}'),
+			' def ',
+			new Token('(', ['a58'], ')'),
+			' kjl',
+		], $tokenizer->tokenize($src));
+		$this->assertEquals($src, implode('', $tokenizer->tokenize($src)));
+	}
+
+
+
+	function testTokenize4() {
+		$src = 'abx {if}ab1 "ahoj {if}" bc2{/} def (a58) kjl';
+		$tokenizer = new Tokenizer([
+			['{if}', '{/}'],
+			['(', ')'],
+		]);
+		$this->assertEquals([
+			'abx ',
+			new Token('{if}', ['ab1 "ahoj {if}" bc2'], '{/}'),
+			' def ',
+			new Token('(', ['a58'], ')'),
+			' kjl',
+		], $tokenizer->tokenize($src));
+		$this->assertEquals($src, implode('', $tokenizer->tokenize($src)));
+	}
+
+
+
+	function _testTokenize5() {
+		// Nefunguje dobře.
+		$src = 'abx {if}ab1 "ahoj {if}"{if}a{/} bc2{/} def (a58) kjl';
+		$tokenizer = new Tokenizer([
+			['{if}', '{/}'],
+			['(', ')'],
+		]);
+		$this->assertEquals([
+			'abx ',
+			new Token('{if}', ['ab1 "ahoj {if}"',
+				new Token('{if}', ['a'], '{/}'),
+				' bc2'], '{/}'),
+			' def ',
+			new Token('(', ['a58'], ')'),
+			' kjl',
+		], $tokenizer->tokenize($src));
+		$this->assertEquals($src, implode('', $tokenizer->tokenize($src)));
+	}
+
+
+
+	/**
+	 * rozdělit i whitechars?
+	 */
+	function testTokenize6() {
+		$src = 'a != 5 AND (b == c OR d > 5) AND id IN (1, 2, 65)';
+		$tokenizer = new Tokenizer([
+			['(', ')'],
+		]);
+		$this->assertEquals([
+			'a != 5 AND ',
+			new Token('(', ['b == c OR d > 5'], ')'),
+			' AND id IN ',
+			new Token('(', ['1, 2, 65'], ')'),
+		], $tokenizer->tokenize($src));
+		$this->assertEquals($src, implode('', $tokenizer->tokenize($src)));
+	}
+
+
+
+	/**
+	 * Ignoruje to parsování prvních závorek.
+	 */
+	function testTokenizeCode() {
+		$src = '
+fn foo(a, b)
+{
+	return a + b;
+}
+
+fn boo(a, b)
+{
+	fn sum(a, b)
+	{
+		return a + b;
+	}
+
+	return a + b + sum(1, 8);
+}
+
+';
+
+		$tokenizer = new Tokenizer([
+			//~ ['(', ')'],
+			['{', '}'],
+		]);
+		$this->assertEquals([
+			"\nfn foo(a, b)\n",
+			new Token('{', ["\n\treturn a + b;\n"], '}'),
+			"\n\nfn boo(a, b)\n",
+			new Token('{', [
+				"\n\tfn sum(a, b)\n\t",
+				new Token('{', ["\n\t\treturn a + b;\n\t"], '}'),
+				"\n\n\treturn a + b + sum(1, 8);\n",
+				//~ new Token('(', ["1, 8"], ')'),
+				//~ ";\n",
+				//~ new Token('{', ["\n\t\treturn a + b;\n\n\treturn a + b + sum(1, 8);\n"], '}'),
+			], '}'),
+			"\n\n",
+		], $tokenizer->tokenize($src));
+		$this->assertEquals($src, implode('', $tokenizer->tokenize($src)));
+	}
+
+
+
+	function testTokenize8() {
+		$src = 'abx {if a=1}ab1 bc2{/} def';
+		$tokenizer = new Tokenizer([
+			['{if', '{/}'],
+		]);
+		$this->assertEquals([
+			'abx ', new Token('{if', [' a=1}ab1 bc2'], '{/}'), ' def'
+		], $tokenizer->tokenize($src));
+		$this->assertEquals($src, implode('', $tokenizer->tokenize($src)));
+	}
+
+
+
+	function testTokenize9() {
+		$src = 'abc(a + b)';
+		$tokenizer = new Tokenizer();
+		$this->assertEquals([
+			'abc',
+			new Token('(', ['a + b'], ')'),
+		], $tokenizer->tokenize($src));
+		$this->assertEquals($src, implode('', $tokenizer->tokenize($src)));
+	}
+
+
+
+	function testTokenize10() {
+		$src = 'abc(a + b(xb))';
+		$tokenizer = new Tokenizer();
+		$this->assertEquals([
+			'abc',
+			new Token('(', ['a + b',
+				new Token('(', ['xb'], ')'),
+			], ')'),
+		], $tokenizer->tokenize($src));
+		$this->assertEquals($src, implode('', $tokenizer->tokenize($src)));
+	}
+
+
+
+	function ___testTokenizeTagAsToken() {
+
+		new Tokenizer([
+			Tokenizer::simple('(', ')'),
+			Tokenizer::tagAsToken('{if', '}', '{/}'),
+		]);
+
+
+		$src = 'abx {if a=1}ab1 bc2{/} def';
+		$tokenizer = new Tokenizer([
+			['{if', '}', '{/}'],
+		]);
+		$this->assertEquals([
+			'abx ',
+			new Token(new Token('{if', 'a=1', '}'), ['ab1 bc2'], '{/}'),
+			' def'
+		], $tokenizer->tokenize($src));
+	}
+
+
+
+	function ___testTokenizeManyEnd() {
+		$src = 'abx {if a=1}ab1 bc2{/if} def';
+		$tokenizer = new Tokenizer([
+			['{if', '}', ['{/}', '{/if}']],
+		]);
+		$this->assertEquals([
+			'abx ',
+			new Token(new Token('{if', 'a=1', '}'), ['ab1 bc2'], '{/if}'),
+			' def'
+		], $tokenizer->tokenize($src));
+	}
+
+
+
+	function __testTokenizeWithBeforeEndDelimiter() {
+		//~ $tokenizer = new Utils\Tokenizer(array(
+			//~ T_DNUMBER => '\d+',
+			//~ T_WHITESPACE => '\s+',
+			//~ T_STRING => '\w+',
+		//~ ));
+
+		//~ $src = 'abx (ab1 bc2) def (a58) kjl';
+		//~ dump($src);
+		//~ dump($tokenizer->tokenize("say \n123"));
+
+		// Tak zrovna tady máme token, který končí začátek následujícího.
+		$input = "
+			@author David 'Grudl
+			@package Nette
+		";
+		$tokenizer = new Tokenizer([
+			['@', Tokenizer::beforeEnd(['@', '*/'])], // token končí některými z těchto tokenů. Ale neslouží jako uzavýrací, nejsou součástí tokenu.
+		]);
+		$this->assertEquals([
+			"\n\t\t\t",
+			new Token('@', ["author David 'Grudl\n\t\t\t"], null),
+			new Token('@', ["package Nette\n\t\t\t"], null),
+		], $tokenizer->tokenize($src));
+
+		$parser = new Parser();
+		$annotations = $parser->parse($input);
+		dump($annotations);
+	}
+
+}
+
+
+
+class Author
+{
+	public $name;
+
+	public function __construct($name)
+	{
+		$this->name = $name;
+	}
+}
+
+
+
+class Package
+{
+	public $name;
+
+	public function __construct($name)
+	{
+		$this->name = $name;
+	}
+}
+
+
+
+class Parser
+{
+	const T_AT = 1;
+	const T_WHITESPACE = 2;
+	const T_STRING = 3;
+
+	/** @var \Nette\Utils\Tokenizer */
+	private $tokenizer;
+
+	/** @var \Nette\Utils\TokenIterator */
+	private $iterator;
+
+	public function __construct()
+	{
+		$this->tokenizer = new Utils\Tokenizer(array(
+			self::T_AT => '@',
+			self::T_WHITESPACE => '\\s+',
+			self::T_STRING => '\\w+',
+		));
+	}
+
+
+
+	public function parse($input)
+	{
+		$this->iterator = new Utils\TokenIterator($this->tokenizer->tokenize($input));
+
+		$result = array();
+		while ($this->iterator->nextToken()) {
+			if ($this->iterator->isCurrent(self::T_AT)) {
+				$result[] = $this->parseAnnotation();
+			}
+		}
+
+		return $result;
+	}
+
+
+
+	protected function parseAnnotation()
+	{
+		$name = __namespace__ . '\\' . ucfirst($this->iterator->joinUntil(self::T_WHITESPACE));
+		$this->iterator->nextUntil(self::T_STRING);
+		$content = $this->iterator->joinUntil(self::T_AT);
+
+		return new $name(trim($content));
+	}
+
+}
